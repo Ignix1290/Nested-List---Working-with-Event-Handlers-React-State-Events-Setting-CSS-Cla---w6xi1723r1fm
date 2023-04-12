548b3da56cc65df7c2380178c0bd6b2d49ba2fd5
Samuel Lakra <samuellakra1290@gmail.com>
2023-04-12 22:01:57 +0530

    1st commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { Component, useState } from "react";
 import "./../styles/App.css";
-
 // Do not alter the states const and values inside it.
 const states = [
   {
@@ -153,9 +152,57 @@ const states = [
     ],
   },
 ];
-
 function App() {
-  return <div id="main"></div>;
+  const [showCities, setShowCities] = useState(0);
+  const [showTown, setShowTowns] = useState(0);
+  function expandState(e) {
+    console.log("state");
+    if(showCities == e.target.id[5]) {
+      setShowCities(0);
+    } else {
+      setShowCities(e.target.id[5])
+    }
+    setShowTowns(0);
+  }
+  function expandCity(e) {
+    console.log("city");
+    e.stopPropagation();
+    if(e.target.id[4] == showTown) {
+      setShowTowns(0);
+    } else {
+      setShowTowns(e.target.id[4])
+    }
+  }
+  return <div id="main">
+    <ul>
+      {states.map((state, index) => {
+        return (
+          <li id={`state${index+1}`} onClick={expandState}>
+            {state.name}
+            {(showCities != 0 && showCities == index + 1) &&
+              <ul>
+                {state.cities.map((city, cIndex) => {
+                    return (
+                      <li id={`city${cIndex+1}`} onClick={expandCity}>
+                        {city.name}
+                        {(showTown != 0 && showTown == cIndex + 1) &&
+                          <ul>
+                            {city.towns.map((town, tIndex) => {
+                              return (
+                                <li id={`town${tIndex+1}`}>{town.name}</li>
+                              );
+                            }) }
+                          </ul>
+                        }
+                      </li>
+                    );
+                })}
+              </ul>
+            }
+          </li>
+        );
+      })}
+    </ul>
+  </div>;
 }
-
 export default App;
